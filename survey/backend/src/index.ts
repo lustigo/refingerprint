34613868cf72f9bd76d5f391a9627146f8fb5d51
Lukas Klassen <luftigo@yahoo.de>
2020-02-05 15:16:26 +0100

    feat: HTTPS Support hinzugefügt

--- survey/backend/src/index.ts
+++ survey/backend/src/index.ts
@@ -1,6 +1,27 @@
+// Load the configuration
+require("dotenv").config();
+
 import koa from "koa";
+import https from "https";
+import fs from "fs";
+
 const app = new koa();
 
+/*
+ * Serve the API 
+ */
+
+/*
+ * Serve the frontend
+ */
 app.use(require("koa-static")("../frontend/dist/"));
 
-app.listen(8080);
+/*
+ * Start the server
+ */
+const options = {
+    key: fs.readFileSync(process.env.TLS_KEY_PATH as string),
+    cert: fs.readFileSync(process.env.TLS_CERT_PATH as string)
+};
+https.createServer(options, app.callback()).listen(process.env.PORT);
+console.log(`Listening on ${process.env.PORT}`);
