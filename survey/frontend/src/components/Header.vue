e8fd7ccdf0c291d3b05079106bc835a2e2ab7276
Lukas Klassen <luftigo@yahoo.de>
2020-02-07 19:23:17 +0100

    fix: Header Formatierung

--- survey/frontend/src/components/Header.vue
+++ survey/frontend/src/components/Header.vue
@@ -1,19 +1,17 @@
 <template>
     <v-toolbar>
-
-  <v-toolbar-items>
+        <v-toolbar-items>
             <v-img src="../assets/logo.png" aspect-ratio="1" width="5rem"/>
-    </v-toolbar-items>
-
-
-            <v-toolbar-title style="margin-left:42%">Umfrage</v-toolbar-title>
-
-
+        </v-toolbar-items>
+        <v-toolbar-title style="margin-left:42%">Umfrage</v-toolbar-title>
     </v-toolbar>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+/**
+ * Navigationbar
+ */
 export default Vue.extend({
     
 });
