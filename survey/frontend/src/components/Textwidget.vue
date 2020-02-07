dc8f085876316c6c7b0f3d38ef5f245610e016a6
Lukas Klassen <luftigo@yahoo.de>
2020-02-07 22:15:38 +0100

    feat: TextWidget hinzugefügt

---
+++ survey/frontend/src/components/Textwidget.vue
@@ -0,0 +1,26 @@
+<template>
+    <vue-markdown v-bind:source="structure.content"/>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueMarkdown from "vue-markdown";
+import TextWidgetDescription from "../interfaces/TextWidgetDescription";
+
+/**
+ * Text-Widget, works with markdown
+ */
+export default Vue.extend({
+    props: ["value"],
+    components: {
+        "vue-markdown": VueMarkdown
+    },
+    data: () => ({
+        structure: {} as TextWidgetDescription,
+    }),
+    mounted() {
+        this.structure = this.value;
+        this.$emit("completed",true);
+    }
+});
+</script>
---
+++ survey/frontend/src/interfaces/TextWidgetDescription.ts
@@ -0,0 +1,7 @@
+/**
+ * Description of the Text-Widget Content
+ */
+export default interface TextWidgetDescription {
+    readonly content: string;
+    // eslint-disable-next-line semi
+}
