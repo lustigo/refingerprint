17b86be5205c01b047c571d26cddefa723644a73
Lukas Klassen <luftigo@yahoo.de>
2020-02-07 22:25:41 +0100

    feat: TextField hinzugefügt

---
+++ survey/frontend/src/components/TextField.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-row>
+        <v-col class="d-flex font-regular font-weight-light text-center" style="margin-left:1%;" cols="6">{{ structure.question }}</v-col>
+        <v-col class="d-flex" cols="4">
+        <v-text-field
+            v-model="structure.content"
+            outlined
+            v-on:change="$emit('completed', !req || structure.content != '')"
+        ></v-text-field>
+        </v-col>
+    </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TextFieldDescription from "../interfaces/TextFieldDescription";
+
+/**
+ * TextField-Widget
+ */
+export default Vue.extend({
+    props: ["value","required"],
+    data: () => ({
+        structure: {} as TextFieldDescription,
+        req: false,
+    }),
+    mounted() {
+        this.structure = this.value;
+        if(!this.structure.content){
+            this.structure.content = "";
+        }
+        this.req = this.required;
+        this.$emit("completed",!this.req || this.structure.content.trim() != "");
+    }
+});
+</script>
---
+++ survey/frontend/src/interfaces/TextFieldDescription.ts
@@ -0,0 +1,8 @@
+/**
+ * Description of the TextField-Widget
+ */
+export default interface TextFieldDescription {
+    readonly question: string;
+    content: string;
+    // eslint-disable-next-line semi
+}
