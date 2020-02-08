ec74cfcd44261709bbde16e0abffc02deae7b222
Lukas Klassen <luftigo@yahoo.de>
2020-02-08 16:26:17 +0100

    feat: Captcha Endpoint am Server

--- survey/backend/src/api.ts
+++ survey/backend/src/api.ts
@@ -3,6 +3,7 @@ import mount from "koa-mount";
 import bodyparser from "koa-bodyparser";
 import fs from "fs";
 import { promisify } from "util";
+import req from "request";
 
 /**
  * Regex for a UUIDv4
@@ -23,6 +24,11 @@ const stat = promisify(fs.stat);
  * Async fs.writeFile
  */
 const writeFile = promisify(fs.writeFile);
+
+/**
+ * Async request
+ */
+const request = promisify(req);
 
 /**
  * Includes the API Routes
@@ -102,6 +108,27 @@ async function saveSurveyData(surveyPath: string, surveyId: string, ctx: Koa.Con
     }
 }
 
+/**
+ * Validate the Captcha Response and sends the result back
+ * @param captchaResponse Response of the Captcha
+ * @param ctx Request-Context
+ */
+async function validateCaptcha(captchaResponse: string, ctx: Koa.Context): Promise<void>{
+    const formData = {
+        secret: process.env.RECAPTCHA_SECRET as string,
+        response: captchaResponse
+    };
+    const response = await request({
+        method: "POST",
+        url: "https://www.google.com/recaptcha/api/siteverify",
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        formData
+    });
+    ctx.status = JSON.parse(response.body).success ? 200: 400;
+}
+
 
 /**
  * Tries to extract the surveyId from the given path
@@ -137,7 +164,27 @@ app.use(bodyparser({
 }));
 
 /**
- * Register the two endpoints
+ * Register the captcha endpoint
+ */
+app.use(mount("/captcha", async(ctx: Koa.Context) => {
+    if (ctx.method != "POST"){
+        ctx.status = 405;
+        ctx.body = "Method not allowed";
+        return;
+    }
+
+    const data = ctx.request.body;
+    if (!data || !data.response) {
+        ctx.body = "Invalid request";
+        ctx.status = 400;
+        return;
+    }
+
+    await validateCaptcha(data.response,ctx);
+}));
+
+/**
+ * Register the survey endpoints
  */
 app.use(mount("/", async (ctx: Koa.Context) => {
     // Check if the Method is GET or POST
