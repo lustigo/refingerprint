f4fbcbc74a8ce23e0ec8412b2fad1c92423b4bc0
Lukas Klassen <luftigo@yahoo.de>
2020-02-26 19:48:09 +0100

    feat: Bildschirmauflösung hinzugefügt

--- ext/src/index.ts
+++ ext/src/index.ts
@@ -4,12 +4,13 @@ import Module from "./interfaces/Module";
 import BrowserInfo from "./modules/BowserInfo";
 import Canvas from "./modules/Canvas";
 import Id from "./modules/Id";
+import ScreenResolution from "./modules/ScreenResolution";
 
 /**
  * All module classes that should be used for the Collection
  * Time-critical modules should be placed at the beginning.
  */
-const modules: Array<{ new(): Module }> = [Canvas, BrowserInfo, Id];
+const modules: Array<{ new(): Module }> = [Canvas, BrowserInfo, ScreenResolution, Id];
 
 /**
  * Start Data Collector
---
+++ ext/src/modules/ScreenResolution.ts
@@ -0,0 +1,37 @@
+import Module from "../interfaces/Module";
+
+/**
+ * Module which stores the Screen Resolution
+ */
+export default class ScreenResolution implements Module {
+
+    /**
+    * Name of the module
+    */
+    public readonly name = "Screen";
+
+
+    /**
+    * Will be called, when the Captcha is rendered
+    * does nothing
+    */
+    public start(): void { }
+
+    /**
+    * Will be called, when the Captcha is solved
+    * does nothing
+    */
+    public stop(): void { }
+
+    /**
+     * returns the Screen Resolution
+     * @returns Screen Resolution
+     */
+    public getCollectedData(): Object {
+        return {
+            width: screen.availWidth,
+            height: screen.availHeight,
+        }
+    }
+
+}
