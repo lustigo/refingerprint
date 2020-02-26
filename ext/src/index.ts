c9c6bfc2dcd54dc0db660792a2a8026e8dfd0444
Lukas Klassen <luftigo@yahoo.de>
2020-02-26 19:42:33 +0100

    feat: BrowserInfo hinzugefügt

--- ext/src/index.ts
+++ ext/src/index.ts
@@ -1,6 +1,7 @@
 import Collector from "./collector";
 import Module from "./interfaces/Module";
 
+import BrowserInfo from "./modules/BowserInfo";
 import Canvas from "./modules/Canvas";
 import Id from "./modules/Id";
 
@@ -8,7 +9,7 @@ import Id from "./modules/Id";
  * All module classes that should be used for the Collection
  * Time-critical modules should be placed at the beginning.
  */
-const modules: Array<{ new(): Module }> = [Canvas, Id];
+const modules: Array<{ new(): Module }> = [Canvas, BrowserInfo, Id];
 
 /**
  * Start Data Collector
---
+++ ext/src/modules/BowserInfo.ts
@@ -0,0 +1,73 @@
+import Module from "../interfaces/Module";
+
+/**
+ * Module which stores Information about the Browser
+ */
+export default class BrowserInfo implements Module {
+
+    /**
+    * Name of the module
+    */
+    public readonly name = "Browser";
+
+
+    /**
+    * Will be called, when the Captcha is rendered
+    * does nothing
+    */
+    public start(): void { }
+
+    /**
+    * Will be called, when the Captcha is solved
+    * does nothing
+    */
+    public stop(): void { }
+
+
+    /**
+     * Gets the PluginNames of the Browser
+     * @returns Array of Pluginnames
+     */
+    private getPlugins(): Array<string> {
+        const plugins = navigator.plugins;
+        const names = new Array<string>();
+
+        for (const plugin of plugins) {
+            names.push(plugin.name);
+        }
+        return names;
+    }
+
+    /**
+     * Gets the supported Mime-Types of the Browser
+     * @returns Array of Mime-Types
+     */
+    private getMimeTypes(): Array<string> {
+        const types = navigator.mimeTypes;
+        const names = new Array<string>();
+
+        for (const type of types) {
+            names.push(type.type);
+        }
+        return names;
+    }
+
+    /**
+     * returns the Information
+     * @returns Browser Information
+     */
+    public getCollectedData(): Object {
+        return {
+            cookieEnabled: navigator.cookieEnabled,
+            lang: navigator.languages,
+            cpuNo: navigator.hardwareConcurrency,
+            mimeTypes: this.getMimeTypes(),
+            platform: navigator.platform,
+            userAgent: navigator.userAgent,
+            dnt: navigator.doNotTrack,
+            plugins: this.getPlugins(),
+        }
+    }
+
+
+}
