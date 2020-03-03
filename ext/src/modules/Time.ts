c9a238fa02f0ae5afbf1ae2053fae5a9836e66ab
Lukas Klassen <luftigo@yahoo.de>
2020-03-03 23:06:31 +0100

    feat: Start und Stop Uhrzeit festhalten

---
+++ ext/src/modules/Time.ts
@@ -0,0 +1,49 @@
+import Module from "../interfaces/Module";
+
+/**
+ * Module which stores the Start- and Endtime
+ */
+export default class Time implements Module {
+    /**
+ * Name of the module
+ */
+    public readonly name = "Time";
+
+    /**
+     * Start-Time
+     */
+    private startTime: Date = null;
+
+    /**
+ * End-Time
+ */
+    private endTime: Date = null;
+
+    /**
+     * Will be called, when the Captcha is rendered
+     * Stores the time
+     */
+    start(): void {
+        this.startTime = new Date();
+    }
+
+    /**
+    * Will be called, when the Captcha is solved
+    * Stores the time
+    */
+    stop(): void {
+        this.endTime = new Date();
+    }
+
+    /**
+     * Returns the Times
+     * @return Start and End-Time as Unix Milliseconds
+     */
+    getCollectedData(): Object {
+        return {
+            start: this.startTime.getTime(),
+            end: this.endTime.getTime(),
+        };
+    }
+
+}
