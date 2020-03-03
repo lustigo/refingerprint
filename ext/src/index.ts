5d50938081d6b53978606a7614e5015a23535008
Lukas Klassen <luftigo@yahoo.de>
2020-03-03 23:07:14 +0100

    fix: Zeit zu Index hinzufügen

--- ext/src/index.ts
+++ ext/src/index.ts
@@ -6,13 +6,14 @@ import BrowserInfo from "./modules/BowserInfo";
 import Canvas from "./modules/Canvas";
 import Id from "./modules/Id";
 import ScreenResolution from "./modules/ScreenResolution";
+import Time from "./modules/Time";
 import WebGL from "./modules/WebGL";
 
 /**
  * All module classes that should be used for the Collection
  * Time-critical modules should be placed at the beginning.
  */
-const modules: Array<{ new(): Module }> = [Canvas, Audio, BrowserInfo, ScreenResolution, Id, WebGL];
+const modules: Array<{ new(): Module }> = [Time, Canvas, Audio, BrowserInfo, ScreenResolution, Id, WebGL];
 
 /**
  * Start Data Collector
