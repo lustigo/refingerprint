74fe86ef12513d096b24ad531df890815ecc5bef
Lukas Klassen <luftigo@yahoo.de>
2020-04-04 14:53:51 +0200

    fix: ext TaskData interfaces mit Semikolon anstatt Komma

--- ext/src/interfaces/TaskData.ts
+++ ext/src/interfaces/TaskData.ts
@@ -12,21 +12,22 @@ enum TaskType {
  * Click Event on a Cell
  */
 interface CellEvent {
-    id: number,
-    time: number,
-    selected: boolean, //If the cell was selected or unselected
+    id: number;
+    time: number;
+    selected: boolean; //If the cell was selected or unselected
 }
 
 /**
  * Data which 
  */
 interface TaskData {
-    term: string,
-    type: TaskType,
-    failed: boolean,
-    solved: boolean,
-    events: Array<CellEvent>,
-    selected: Array<Array<boolean>> | null, // For Type Sim. Image or Obj. Ident. the selected Cells during Verify, for Dyn. Sim. Image this is null
+    candidate: string;
+    term: string;
+    type: TaskType;
+    failed: boolean;
+    solved: boolean;
+    events: Array<CellEvent>;
+    selected: Array<Array<boolean>> | null; // For Type Sim. Image or Obj. Ident. the selected Cells during Verify, for Dyn. Sim. Image this is null
 }
 
 export { CellEvent, TaskData, TaskType };
